55133901b6f5fff08b691648bdbc5b5ccb26d1cf
gabybertorello <gibertorello@gmail.com>
2016-09-01 14:15:55 -0300

    Main.java

--- src/ctdsCompiler/Main.java
+++ src/ctdsCompiler/Main.java
@@ -1,19 +1,6 @@
 package ctdsCompiler;
 
-/*
-File Name: Main.java
-To Create: 
-After .flex, and .cup, have been created.
-> javac Main.java
-
-To Run: 
-> java Main input.test
-
-*/
-
 import java.io.*;
-//import java_cup.Lexer;
-//import java_cup.parser;
  
 public class Main {
 static public void main(String argv[]) {    
