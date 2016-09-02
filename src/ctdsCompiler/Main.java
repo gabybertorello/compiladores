26954855256a3d04f4d2ac3e7042dd866945e3fc
gabybertorello <gibertorello@gmail.com>
2016-09-02 14:59:07 -0300

    main andando

--- src/ctdsCompiler/Main.java
+++ src/ctdsCompiler/Main.java
@@ -6,15 +6,14 @@ import java_cup.runtime.*;
 public class Main {
 	static public void main(String args[])throws Exception {    
 	  try {
-			ComplexSymbolFactory sf = new ComplexSymbolFactory();
 			if (args.length==0){
 				Lexer l = new Lexer (System.in);
-				parser p = new parser (l,sf);
+				parser p = new parser (l);
 				p.parse();
 				System.out.print("HOLA");
 			}else{
 				Lexer l = new Lexer (new java.io.FileInputStream(args[0]));
-				parser p = new parser (l,sf);
+				parser p = new parser (l);
 				p.parse(); 
 			}    
 	  }catch (Exception e) {
